e12aa3592413ac49a5386ad2095a397045b9ee03
jethanivijay <66484052+jethanivijay@users.noreply.github.com>
2020-11-24 15:16:04 +0530

    Update Startup.cs

--- src/Contoso.WebApi/Startup.cs
+++ src/Contoso.WebApi/Startup.cs
@@ -23,7 +23,7 @@ namespace Contoso.WebApi
 
 
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container..
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(options =>
